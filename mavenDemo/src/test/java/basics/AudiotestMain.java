f2f8bb2de26dfdaf8359b9b340f80740d8a5231b
739926190@qq.com <19881015>
2017-03-15 17:36:20 +0800

    asdad

---
+++ mavenDemo/src/test/java/basics/AudiotestMain.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class AudiotestMain {
+	
+	public static void main(String[] args) {
+		Audiotest at = new Audiotest("./d.wav");
+		at.start();
+	}
+
+}
---
+++ mavenDemo/src/test/java/effective/chapter1/Person.java
@@ -0,0 +1,21 @@
+package effective.chapter1;
+
+/**
+ * 人员多个构造器参数
+ * 
+ * @author wujl
+ */
+public class Person {
+	
+	private String name ; // 姓名
+	
+	private int age ; // 年龄
+	
+	private String sex ; // 性别
+	
+	private String job ; // 工作
+	
+	private String interest ; // 兴趣
+
+	private String nativePlace ; // 籍贯
+}
